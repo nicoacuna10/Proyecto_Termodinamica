5e3630ba647d8182a84c27f0384d99beeb09d7b4
Nicolás Acuña <80644036+nicoacuna10@users.noreply.github.com>
2021-11-29 22:54:26 -0300

    Update CalculadoraDeHuellaDeCarbonoPersonal.c

--- CalculadoraDeHuellaDeCarbonoPersonal.c
+++ CalculadoraDeHuellaDeCarbonoPersonal.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-#include <stdbool.h>
-#include <ctype.h>
 
 int numeroPersonasHogar;
 
 void MensajeDeBienvenida(){
+
+    printf(" -------------------------------------------\n");
+    printf("| CALCULADORA DE HUELLA DE CARBONO PERSONAL |\n");
+    printf(" -------------------------------------------\n");
 
     // Mensaje
 
@@ -12,18 +14,22 @@ void MensajeDeBienvenida(){
 
 float EnergiaEnElHogar(){
 
+    printf("\n ----------------------------\n");
+    printf("| Eje 1: Energia en el hogar |\n");
+    printf(" ----------------------------\n");
+
     float emision = 0;
     float dato_de_entrada;
 
-    printf("1. ¿Cuanto es el consumo en su hogar (en kWh) de electricidad al mes?\n");
+    printf("\n1. ¿Cuanto es el consumo en su hogar (en kWh) de electricidad al mes?\n");
     scanf("%f", &dato_de_entrada);
     emision += (dato_de_entrada * 0.4195 * 12);
 
-    printf("2. ¿Cuanto es el consumo en su hogar (en metros cúbicos) de gas natural al mes?\n");
+    printf("\n2. ¿Cuanto es el consumo en su hogar (en metros cúbicos) de gas natural al mes?\n");
     scanf("%f", &dato_de_entrada);
     emision += (dato_de_entrada * 2.097 * 12);
 
-    printf("3. ¿Cuanto el consumo en su hogar (en kilogramos) de gas licuado al mes?\n");
+    printf("\n3. ¿Cuanto es el consumo en su hogar (en kilogramos) de gas licuado al mes?\n");
     scanf("%f", &dato_de_entrada);
     emision += (dato_de_entrada * 2.957 * 12);
 
@@ -35,14 +41,18 @@ float EnergiaEnElHogar(){
 
 float EnergiaEnElTransporte(){
 
+    printf("\n ---------------------------------\n");
+    printf("| Eje 2: Energia en el transporte |\n");
+    printf(" ---------------------------------\n");
+
     float emision = 0;
     float dato_de_entrada;
 
-    printf("1. ¿Cuantos kiometros recorre en auto al mes?\n");
+    printf("\n1. ¿Cuantos kilometros recorre en auto al mes?\n");
     scanf("%f", &dato_de_entrada);
     emision += (dato_de_entrada * 0.18568 * 12);
 
-    printf("2. Si viaja en avión ¿Cuantos kilometros recorre al año?\n");
+    printf("\n2. Si viaja en avión ¿Cuantos kilometros recorre al año?\n");
     scanf("%f", &dato_de_entrada);
     emision += (dato_de_entrada * 0.285);
 
@@ -52,32 +62,36 @@ float EnergiaEnElTransporte(){
 
 float HabitosDeConsumo(){
 
+    printf("\n ---------------------------\n");
+    printf("| Eje 3: Habitos de consumo |\n");
+    printf(" ---------------------------\n");
+
     float emision = 0;
     float dato_de_entrada;
 
-    printf("1. ¿Cuantos kilogramos de carne de vacuno consume a la semana?\n");
+    printf("\n1. ¿Cuantos kilogramos de carne de vacuno consume a la semana?\n");
     scanf("%f", &dato_de_entrada);
     emision += (dato_de_entrada * 26.5 * 52);
 
-    printf("2. ¿Cuantos kilogramos de carne de cerdo consume a la semana?\n");
+    printf("\n2. ¿Cuantos kilogramos de carne de cerdo consume a la semana?\n");
     scanf("%f", &dato_de_entrada);
     emision += (dato_de_entrada * 1.5 * 52);
 
-    printf("3. ¿Cuantos kilogramos de carne de ave consume a la semana?\n");
+    printf("\n3. ¿Cuantos kilogramos de carne de ave consume a la semana?\n");
     scanf("%f", &dato_de_entrada);
     emision += (dato_de_entrada * 0.6 * 52);
 
-    printf("4. ¿Cuantos litros de leche consume a la semana?\n");
+    printf("\n4. ¿Cuantos litros de leche consume a la semana?\n");
     scanf("%f", &dato_de_entrada);
     emision += (dato_de_entrada * 0.874 * 52);
 
-    printf("5. ¿Cuantos huevos consume a la semana?\n");
+    printf("\n5. ¿Cuantos huevos consume a la semana?\n");
     scanf("%f", &dato_de_entrada);
     emision += (dato_de_entrada * 0.04 * 52);
 
-    printf("6. ¿Cual es el consumo en su hogar (en metros cúbicos) de agua al mes?\n");
+    printf("\n6. ¿Cual es el consumo en su hogar (en metros cúbicos) de agua al mes?\n");
     scanf("%f", &dato_de_entrada);
-    emision += (dato_de_entrada * 0.708 * 12);
+    emision += (dato_de_entrada * 0.708 * 12 / 4);
 
     return emision;
 
@@ -92,49 +106,34 @@ int main(){
     float emisionTransporte;
     float emisionHabitos;
     
-    char opcion[10];
-    bool permanecer_en_calculadora = true;
 
     MensajeDeBienvenida();
 
-    while(permanecer_en_calculadora){
 
-        emisionTotalAnual = 0;
+    emisionTotalAnual = 0;
 
-        printf("\n¿Cuantas personas viven en su hogar?\n");
-        scanf("%d", &numeroPersonasHogar);
+    printf("\n¿Cuantas personas viven en su hogar?\n");
+    scanf("%d", &numeroPersonasHogar);
 
-        emisionHogar = EnergiaEnElHogar();
-        emisionTransporte = EnergiaEnElTransporte();
-        emisionHabitos = HabitosDeConsumo();
+    emisionHogar = EnergiaEnElHogar();
+    emisionTransporte = EnergiaEnElTransporte();
+    emisionHabitos = HabitosDeConsumo();
 
-        emisionTotalAnual = emisionHogar + emisionTransporte + emisionHabitos;
+    emisionTotalAnual = emisionHogar + emisionTransporte + emisionHabitos;
+    
+    printf("\nEmisiones en el eje \"Energia en el hogar\": %f (kgCO2eq)\n", emisionHogar);
+    printf("\nEmisiones en el eje \"Energia en el transporte\": %f (kgCO2eq)\n", emisionTransporte);
+    printf("\nEmisiones en el eje \"Habitos de consumo\": %f (kgCO2eq)\n", emisionHabitos);
+    printf("\nEmisiones en Total: %f (kgCO2eq)\n", emisionTotalAnual);
+
+    if(emisionTotalAnual < 5000){
         
-        printf("%f\n", emisionHogar);
-        printf("%f\n", emisionTransporte);
-        printf("%f\n", emisionHogar);
-        printf("%f\n", emisionTotalAnual);
+    }else{
 
-        if(emisionTotalAnual < 5000){
-            
-        }else{
-
-            // Recomendaciones para disminuir las emisiones
-        }
-
-        printf("Desea reiniciar la calculadora?\n");
-        printf("Escriba SI para reiniciar o NO para salir de la calculadora\n");
-        scanf("%s", opcion);
-
-        for(int i = 0; opcion[i]; i++){
-            opcion[i] = toupper(opcion[i]);
-        }
-
-        if(opcion[0] == 'N' && opcion[1] == 'O'){
-            permanecer_en_calculadora = false;
-        }
-
+        // Recomendaciones para disminuir las emisiones
     }
+
+
 
     return 0;
 }
